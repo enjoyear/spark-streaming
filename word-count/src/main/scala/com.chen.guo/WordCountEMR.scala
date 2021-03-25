f4e6ba3cac543a9b852450a746b48f62360059fb
Chen Guo <almightygc@gmail.com>
2021-03-25 13:01:44 -0700

    add dummy word count example

---
+++ word-count/src/main/scala/com.chen.guo/WordCountEMR.scala
@@ -0,0 +1,33 @@
+package com.chen.guo
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object WordCountEMR extends App {
+  val spark = SparkSession
+    .builder
+    .master("local[*]")
+    .appName("WordCount")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("WARN")
+
+  val inputPath = "/Users/chen.guo@coinbase.com/repo/enjoyear/spark-streaming/word-count/test-file/word-count.txt"
+
+  val data: RDD[String] = spark.sparkContext.textFile(inputPath)
+  //Print out the RDD to verify that file can be loaded correctly
+  data.foreach(println(_))
+  val splitdata = data.flatMap(line => line.split(" "));
+  // The type of this map data is RDD[(String, Int)]
+
+  import spark.implicits._
+
+  val df: DataFrame = splitdata.map(word => (word, 1)).toDF("word", "count")
+  df.groupBy("word")
+    .agg(sum("count").as("count"))
+    .orderBy(desc("count"))
+    .limit(100)
+    .collect()
+    .foreach(println)
+}
+
