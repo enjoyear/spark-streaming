328f876ab8a32455d71d6691bcfb7363db912b1a
Chen Guo <almightygc@gmail.com>
2021-04-01 14:12:49 -0700

    test

---
+++ word-count/src/main/scala/com.chen.guo/WordCountDatabricks.scala
@@ -0,0 +1,41 @@
+package com.chen.guo
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.{desc, sum}
+
+object WordCountDatabricks {
+  val outputPath = "/tmp/chenguo/test2"
+
+  def main(args: Array[String]): Unit = {
+    for (arg <- args) {
+      println(s"Got arg: ${arg}")
+    }
+
+    val spark = SparkSession
+      .builder
+      .appName("WordCount")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+    val df = spark.table("chen_guo.diamonds")
+    df.write.mode("overwrite").json(outputPath)
+    println(s"writtent to ${outputPath}")
+
+
+    //Word Count Example
+    val logData: RDD[String] = spark.sparkContext.textFile("s3a://cb-databricks-logs-development/0326-210651-davit416/driver/log4j-active.log")
+    val splitdata = logData.flatMap(line => line.split(" "));
+
+    import spark.implicits._
+
+    val logDf: DataFrame = splitdata.map(word => (word, 1)).toDF("word", "count")
+    logDf.groupBy("word")
+      .agg(sum("count").as("count"))
+      .orderBy(desc("count"))
+      .limit(100)
+      .collect()
+      .foreach(println)
+  }
+}
+
