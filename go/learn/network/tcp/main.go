bdc35d21dd8dcbbf28f8dc8b3d9aad9640cf75d4
Chen Guo <almightygc@gmail.com>
2021-07-15 19:54:11 -0700

    updates

--- go/learn/network/tcp/main.go
+++ go/learn/network/tcp/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
 	//Connect locally using "telnet localhost 2001"
-	port := 2000
-	for {
-		port++
+	for port := 2000; port < 2005; port++ {
 		fmt.Printf("Port is %v\n", port)
 		listenAddress := fmt.Sprintf("localhost:%d", port)
 		l, err := net.Listen("tcp", listenAddress)
@@ -18,16 +17,24 @@ func main() {
 			return
 		}
 
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Printf("Error in accepting on %v: %v\n", listenAddress, err)
-			l.Close()
-		}
-		tcpConn := conn.(*net.TCPConn)
-		fmt.Printf("%s\n", tcpConn)
-		fmt.Printf("Local Address: %s\n", tcpConn.LocalAddr())
-		fmt.Printf("Remote Address: %s\n", tcpConn.RemoteAddr())
-		tcpConn.SetKeepAlive(true)
-		//tcpConn.SetKeepAlive(true)
+		go func() {
+			for {
+				fmt.Printf("Waiting on %s\n", listenAddress)
+				conn, err := l.Accept()
+				if err != nil {
+					fmt.Printf("Error in accepting on %v: %v\n", listenAddress, err)
+					l.Close()
+					return
+				}
+				tcpConn := conn.(*net.TCPConn)
+				fmt.Printf("Build new connection for %s\n", tcpConn)
+				fmt.Printf("Local Address: %s\n", tcpConn.LocalAddr())
+				fmt.Printf("Remote Address: %s\n", tcpConn.RemoteAddr())
+				tcpConn.SetKeepAlive(true)
+				//tcpConn.SetKeepAlive(true)
+			}
+		}()
 	}
+
+	time.Sleep(1000 * time.Second)
 }
