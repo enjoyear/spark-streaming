536c0f4e52e213469574a5e220ac37f33f1aab97
Chen Guo <almightygc@gmail.com>
2021-12-11 23:00:57 -0800

    data sketches

--- spark-kafka/src/main/scala/com/chen/guo/db/postgres/PostgresIngest.scala
+++ spark-kafka/src/main/scala/com/chen/guo/db/postgres/PostgresIngest.scala
@@ -1,6 +1,11 @@
 package com.chen.guo.db.postgres
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.datasketches.theta.{PairwiseSetOperations, UpdateSketch}
+import org.apache.spark.api.java.function.ReduceFunction
+import org.apache.spark.sql.{DataFrame, Dataset, Encoders, Row, SparkSession}
+
+import java.util
+
 
 object PostgresIngest extends App {
   val spark = SparkSession
@@ -18,23 +23,50 @@ object PostgresIngest extends App {
 
   val query = "SELECT id FROM transactions"
 
-  def withPartitionColumn: DataFrame = {
-    val reader = spark.read
-      .format("jdbc")
-      .option("url", s"jdbc:postgresql://$hostName:5432/$dbName")
-      .option("user", userName)
-      .option("password", password)
-      .option("dbtable", s"(${query}) as A")
-      .option("partitionColumn", "id")
-      .option("lowerBound", 1086710) //1068019, 1086719
-      .option("upperBound", 1086730) //1068030, 1101683
-      .option("numPartitions", 5)
-      .option("fetchSize", 10000)
-      .option("driver", "org.postgresql.Driver")
+  val initialValue: ThetaSketchJavaSerializable = new ThetaSketchJavaSerializable();
 
-    reader.load()
-  }
+  val reader = spark.read
+    .format("jdbc")
+    .option("url", s"jdbc:postgresql://$hostName:5432/$dbName")
+    .option("user", userName)
+    .option("password", password)
+    .option("dbtable", s"(${query}) as src_table")
+    .option("partitionColumn", "id")
+    .option("lowerBound", 6) //1068019
+    .option("upperBound", 1101675) //1068030
+    .option("numPartitions", 5)
+    .option("fetchSize", 10000)
+    .option("driver", "org.postgresql.Driver")
 
-  withPartitionColumn.write.mode("overwrite").csv(s"/tmp/postgres/csv")
+  val df: DataFrame = reader.load()
+  df.printSchema()
 
+  print("Table size: " + df.count())
+
+  // df.write.mode("overwrite").csv(s"/tmp/postgres/csv")
+
+  val mappedData: Dataset[ThetaSketchJavaSerializable] = df.mapPartitions((it: util.Iterator[Row]) => {
+    val sketch = new ThetaSketchJavaSerializable
+    while (it.hasNext) {
+      sketch.update(it.next.get(0).toString)
+    }
+    util.Arrays.asList(sketch).iterator
+  }, Encoders.javaSerialization(classOf[ThetaSketchJavaSerializable]))
+
+  val sketch: ThetaSketchJavaSerializable = mappedData.reduce(new ReduceFunction[ThetaSketchJavaSerializable]() {
+    @throws[Exception]
+    override def call(sketch1: ThetaSketchJavaSerializable, sketch2: ThetaSketchJavaSerializable): ThetaSketchJavaSerializable = {
+      if (sketch1.getSketch == null && sketch2.getSketch == null)
+        return new ThetaSketchJavaSerializable(UpdateSketch.builder.build.compact)
+      if (sketch1.getSketch == null)
+        return sketch2
+      if (sketch2.getSketch == null)
+        return sketch1
+      val compactSketch1 = sketch1.getCompactSketch
+      val compactSketch2 = sketch2.getCompactSketch
+      new ThetaSketchJavaSerializable(PairwiseSetOperations.union(compactSketch1, compactSketch2))
+    }
+  })
+
+  println(s"Unique count: ${sketch.getEstimate}")
 }
