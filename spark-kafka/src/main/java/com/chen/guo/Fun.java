2396587d35221c7bd739aac97c5fe94c9d83e84d
Chen Guo <almightygc@gmail.com>
2022-05-07 15:59:17 -0700

    update

--- spark-kafka/src/main/java/com/chen/guo/Fun.java
+++ spark-kafka/src/main/java/com/chen/guo/Fun.java
@@ -2,7 +2,7 @@ package com.chen.guo;
 
 public class Fun {
     public static void main(String[] args) {
-        System.out.println("hi ");
+        System.out.println("hi this is a valid");
     }
     // test 3
 }
