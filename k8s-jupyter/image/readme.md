fece375a2ec75acd2b0c359cee4fdf5d783baee6
Chen Guo <almightygc@gmail.com>
2025-11-15 22:10:02 -0800

    update

---
+++ k8s-jupyter/image/readme.md
@@ -0,0 +1,14 @@
+```bash
+# build the image with a tag
+# the last `.` indicates the context path, which controls what files are visible to the Dockerfile during build (e.g. what COPY/ADD can see)
+docker build -t jupyter-scala:v1 -f ./k8s-jupyter/image/Dockerfile .
+
+# Verify the image was created
+docker images | grep jupyter-scala
+docker images --digests
+docker image inspect jupyter-scala:v1
+
+# Load the Image into kind
+# --name part is for kind cluster name (`kind get clusters`)
+kind load docker-image spark-wordcount:v1 --name prod-test
+```
---
+++ word-count/src/main/scala/com.chen.guo/WordCountSparkConnect.scala
@@ -0,0 +1,38 @@
+package com.chen.guo
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object WordCountSparkConnect extends App {
+  val argumentList: Array[String] = args
+  argumentList.foreach(arg => println(s"Received argument: $arg"))
+
+  val spark = SparkSession
+    .builder
+    .remote("sc://all-purpose1-svc.spark-operator.svc.cluster.local:15002")
+    .appName("WordCount")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("WARN")
+
+  // val inputPath = "s3a://chen-guo-test/test/input/word-count.txt"
+  val inputPath = "file:///etc/passwd"
+
+  val data: RDD[String] = spark.sparkContext.textFile(inputPath)
+  //Print out the RDD to verify that file can be loaded correctly
+  data.foreach(println(_))
+  val splitdata = data.flatMap(line => line.split(" "));
+  // The type of this map data is RDD[(String, Int)]
+
+  import spark.implicits._
+
+  val df: DataFrame = splitdata.map(word => (word, 1)).toDF("word", "count")
+  df.groupBy("word")
+    .agg(sum("count").as("count"))
+    .orderBy(desc("count"))
+    .limit(100)
+    .collect()
+    .foreach(println)
+}
+
